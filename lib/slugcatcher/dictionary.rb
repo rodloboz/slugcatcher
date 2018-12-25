e1b33d4e8794b34815cc5641978c41471d32c38d
Rui Freitas <dinnerpartysaboteur@gmail.com>
2018-12-25 17:26:30 +0000

    get all dictionaries

--- lib/slugcatcher/dictionary.rb
+++ lib/slugcatcher/dictionary.rb
@@ -1,30 +1,39 @@
 module Slugcatcher
   class Dictionary
     def initialize(models)
-      models.each do |model|
-        Dictionary.define_dictionary_methods(model)
+      @names = models.map { |m| m.name.underscore}
+      @names.each do |name|
+        Dictionary.define_dictionary_methods(name)
       end
     end
 
-    def self.define_dictionary_methods(model)
-      attr_name = model.name.underscore
-      dictionary_name = "#{attr_name.pluralize}_dictionary"
+    def slug_dictionary
+      cuisines_dictionary.merge(locations_dictionary).merge(categories_dictionary)
+    end
+
+    def self.define_dictionary_methods(name)
+      dictionary_name = "#{name.pluralize}_dictionary"
 
       define_method(dictionary_name) do
         instance_variable_set("@#{dictionary_name}", build_dictionary(name))
       end
 
-      define_method("#{attr_name}?") do |term|
+      define_method("#{name}?") do |term|
         send(dictionary_name).key?(term)
       end
     end
 
+    def dictionaries
+      @dictionaries ||= methods.grep /^.*_dictionary/
+    end
+
     private
+
 
     def build_dictionary(name)
       puts "building dictionary"
       dictionary = {}
-      klass = name.to_s.camelize.constantize
+      klass = name.camelize.constantize
       klass.pluck(:id, :name).each do |id, name|
         key = name.parameterize
         dictionary[key] = {id: id, text: name }
